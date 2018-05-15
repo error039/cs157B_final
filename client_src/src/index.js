e37979e9e0d22eb59b5130e38164a5b06cb3d046
error039 <ruanzihang1@gmail.com>
2018-05-14 18:55:05 -0700

    added router

--- client_src/src/index.js
+++ client_src/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Router, Route, Switch } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 import App from './App';
+import history from './history';
+import Register from './Register';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+  return (
+    <CookiesProvider>
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={App}/>
+          <Route path="/Register" component={Register}/>
+        </Switch>
+      </Router>
+    </CookiesProvider>
+  );
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
